652b7ceca593ce6932e37c6c909c27eae1274adb
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-03-30 15:05:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env."ZIl18Wf_C3WXeRbQX7Q_YV2Q6JELD22x");//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
