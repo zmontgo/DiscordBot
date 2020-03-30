3c20134fec34f3340ad39f0b2c9ec1587266fb41
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-03-30 14:53:34 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env."ZIl18Wf_C3WXeRbQX7Q_YV2Q6JELD22x");//BOT_TOKEN is the Client Secret
